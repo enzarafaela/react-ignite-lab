e5b066741e880c4dccb5709f1ca3a0e72c7c47d2
Enza Rafaela De Nadai Victal <rafaeladenadai@gmail.com>
2022-10-16 01:28:48 -0300

    Aula02 - Criando e Alimentando Storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     fontSize: {
       xs: 14,
@@ -8,22 +8,31 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      "2xl": 32,
+      '2xl': 32,
     },
 
     colors: {
-      "gray-100": "#E1E1E6",
-      "gray-200": "#7C7C7A",
-      "gray-400": "#7C7C8A",
-      "gray-800": "#202024",
-      "gray-900": "#121214",
-      "gray-100": "#E1E1E6",
-      "cyan-500": "#81DFF7",
-      "cyan-300": "#B2E4F5",
+      trasnparent: 'transparent',
+      black: '#000',
+      white: '#FFF',
+
+      gray: {
+        100: '#E1E1E6',
+        200: '#7C7C7A',
+        400: '#7C7C8A',
+        800: '#202024',
+        900: '#121214',
+        100: '#E1E1E6'
+      },
+
+      cyan: {
+        500: '#81DFF7',
+        300: '#B2E4F5'
+      },
     },
     extend: {
       fontFamily: {
-        sans: "Inter, sans-serif",
+        sans: 'Inter, sans-serif',
       },
     },
   },
