84c9df53882a19b39757becb774c185ac0b73eb4
Enza Rafaela De Nadai Victal <rafaeladenadai@gmail.com>
2022-10-18 23:48:13 -0300

    Aula 3 - Testando e Automatizando

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Heading } from './components/Heading';
 import { Text } from './components/Text';
 import { TextInput } from './components/TextInput';
 import { Envelope, Lock } from 'phosphor-react'
-import Logo from './Logo';
+import { Logo }  from './Logo';
 import './styles/global.css';
 import { Checkbox } from './components/Checkbox';
 import { Button } from './components/Button';
@@ -13,14 +13,16 @@ export function App() {
       <header className="flex flex-col items-center justify-center">
         <Logo />
 
-        <Heading>Ignite Lab</Heading>
+        <Heading size="lg" className="mt-1">
+          Ignite Lab
+        </Heading>
 
         <Text size="md" className="text-gray-400">
           Faça login e comece a usar!
         </Text>
       </header>
 
-      <form className="flex flex-col items-stretch w-full max-w-sm mt-10">
+      <form className="flex flex-col items-stretch w-full max-w-sm mt-10 gap-4">
         <label htmlFor="email" className="flex flex-col gap-3">
           <Text size="sm" className="font-semibold">
             Endereço de e-mail:
@@ -31,7 +33,9 @@ export function App() {
               <Envelope />
             </TextInput.Icon>
 
-            <TextInput.Input placeholder="Type your e-mail adress" />
+            <TextInput.Input
+              type="email" id='email' placeholder="Type your e-mail adress"
+            />
           </TextInput.Root>
         </label>
 
@@ -45,26 +49,32 @@ export function App() {
               <Lock />
             </TextInput.Icon>
 
-            <TextInput.Input placeholder="**********" />
+            <TextInput.Input type="password" id='password' placeholder="**********" />
           </TextInput.Root>
         </label>
 
         <label htmlFor="remember" className="flex items-center gap-2 mt-3">
-          <Checkbox />
+          <Checkbox id="remember" />
           <Text size="sm" className="text-gray-400">
             Lembrar de mim por 30 dias
           </Text>
         </label>
 
-        <Button type='submit' className='mt-4'> Entrar na plataforma </Button>
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
+        </Button>
       </form>
 
       <footer className="flex flex-col items-center gap-4 mt-8">
-        <Text size="sm" className="text-gray-400">
-          Esqueceu sua senha?
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline hover:text-gray-100">
+            Esqueceu sua senha
+          </a>
         </Text>
-        <Text size="sm" className="text-gray-400">          
-          Não possui conta? Crie uma agora!
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline hover:text-gray-100">
+            Não possui conta? Crie uma agora!
+          </a>
         </Text>
       </footer>
     </div>
